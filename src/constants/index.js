4abb92876b55ed4374a770dfd9c2b3f0ed5b429d
OlukaGibson <gibsonoluka7@gmail.com>
2025-03-06 07:41:53 +0300

    code clean

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,4 @@
 // import ProductImage13 from "../laptops/brand/acerpc1.jpeg"
 
 export const WELCOME_MESSAGE = "Developed to obtain, control and manage data from IoT devices. This platform is designed to be user-friendly and efficient for all users."; 
+
